5e82086f7ed80794b526326b2920ba10ad5da466
louhezhan <736097782@qq.com>
2023-12-05 16:28:45 +0800

    fix test id

--- config/index.cjs
+++ config/index.cjs
@@ -21,7 +21,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '旅行者',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '59736de9ee474e3fb3ac4b154e6402d7',
+      id: 'oEQgq6scgwqvQvo9aAoVs2I0zZbI',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: '0001',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
